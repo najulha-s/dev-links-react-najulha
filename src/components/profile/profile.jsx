e9b3f1e600141974cf526bd0e33a63aaa8f249c8
Annycfranco <annych.franco@gmail.com>
2023-08-10 17:13:19 -0300

    finalizando aula 10/08

---
+++ src/components/profile/profile.jsx
@@ -0,0 +1,8 @@
+export default function profile(children){
+    return(
+        <div className="profile">
+            <img src="" alt=""></img>
+            <p>@najulha</p>
+        </div>
+    )
+}
